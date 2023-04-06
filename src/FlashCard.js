44bf32137437c931c3521e42e77257f1fee60a69
onanuviie <nuviesilvet@gmail.com>
2023-04-06 06:32:29 +0100

    styled the cards

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -4,8 +4,20 @@ export default function FlashCard({ flashCard }) {
   const [flip, setFlip] = useState(false)
 
   return (
-    <div onClick={() => setFlip(!flip)}>
-      {flashCard.question}
+    <div 
+      className = {`card ${flip ? 'flip' : ''}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className = "front">
+        {flashCard.question}
+        <div className = "flashcard-options">
+          {flashCard.options.map(option => {
+            return <div className = "flashcard-option">{option}</div>
+          })}
+        </div>
+      </div>
+
+      <div className = "back">{flashCard.answer}</div>
     </div>
   )
 }
