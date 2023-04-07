fc89c98f07106eb4127bc1e2643727b9f0719a6e
onanuviie <nuviesilvet@gmail.com>
2023-04-08 00:29:59 +0100

    responsive heights

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function FlashCard({ flashCard }) {
   const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
+
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = frontEl.current.getBoundingClientRect().height
+
+    setHeight(Math.max(frontHeight, backHeight, 100))
+  }
+
+  useEffect(() => {
+    setMaxHeight()
+  }, [flashCard.question, flashCard.answer, flashCard.options])
+
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+    return (() => window.removeEventListener('resize', setMaxHeight))
+  })
 
   return (
     <div 
       className = {`card ${flip ? 'flip' : ''}`}
+      style = {{height: height}}
       onClick={() => setFlip(!flip)}
     >
-      <div className = "front">
+      <div className = "front" ref = {frontEl}>
         {flashCard.question}
         <div className = "flashcard-options">
           {flashCard.options.map(option => {
@@ -17,7 +38,7 @@ export default function FlashCard({ flashCard }) {
         </div>
       </div>
 
-      <div className = "back">{flashCard.answer}</div>
+      <div className = "back" ref = {backEl}>{flashCard.answer}</div>
     </div>
   )
 }
